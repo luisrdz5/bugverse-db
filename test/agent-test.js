6a3b7648089565771b32fc58cf768479b8018d66
th3_bu6 <th3_bu6@lap.huawei.net>
2018-03-13 17:16:26 -0600

    we agregate agent functionalities to test the objects what i need

--- test/agent-test.js
+++ test/agent-test.js
@@ -3,6 +3,9 @@
 const test = require('ava')
 const proxyquire = require('proxyquire')
 const sinon = require('sinon')
+
+const agentFixtures = require('./fixtures/agent')
+
 let config = {
   logging: function () {
 
@@ -12,8 +15,9 @@ let config = {
 let MetricStub = {
   belongsTo: sinon.spy()
 }
+let single = Object.assign({}, agentFixtures.single)
+let id = 1
 let AgentStub = null
-
 let db = null
 let sandbox = null
 
@@ -22,6 +26,10 @@ test.beforeEach(async () => {
   AgentStub = {
     hasMany: sandbox.spy()
   }
+  // Here starts the  findById Stub
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
     './models/metric': () => MetricStub
@@ -40,4 +48,13 @@ test.serial('Setup', t => {
   t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
   t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was Executed')
   t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+})
+test.serial('Agent#findById', async t => {
+  let agent = await db.Agent.findById(id)
+
+  t.true(AgentStub.findById.called, 'findById should be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById should be called once')
+  t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
+
+  t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
 })
